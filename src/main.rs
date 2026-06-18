a9f1744f2f0a0df266d4823dfaa4c1008bc2355a
aaronvigil96 <aaronvigil96@gmail.com>
2026-06-17 22:03:13 -0300

    adding more changes

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use std::{io, vec};
+use std::{
+    io::{self, stdin},
+    vec,
+};
 
 struct User {
     id: u8,
@@ -6,25 +9,28 @@ struct User {
     lastname: String,
     email: String,
     age: u8,
-    active: bool
+    active: bool,
 }
 
 fn main() {
-    let mut users:Vec<User> = Vec::new();
+    let mut users: Vec<User> = Vec::new();
 
-    let mut menu_running:bool = true;
+    let mut menu_running: bool = true;
     let mut option = String::new();
 
     while menu_running {
         println!("¿Que desea hacer?");
         println!("1)- Crear usuario");
         println!("2)- Listar usuarios");
+        println!("3)- Activar/desactivar usuario");
         println!("0)- Salir");
 
         let input = loop {
             option.clear();
 
-            io::stdin().read_line(&mut option).expect("Error al ingresar opción");
+            io::stdin()
+                .read_line(&mut option)
+                .expect("Error al ingresar opción");
             match option.trim().parse::<u8>() {
                 Ok(number) => break number,
                 Err(_) => {
@@ -38,23 +44,28 @@ fn main() {
             1 => {
                 let user: User = create_user(&users);
                 users.push(user);
-            },
+            }
             2 => {
                 list_users(&users);
             }
+            3 => {
+                toggle_status(&mut users);
+            }
             0 => {
                 menu_running = false;
-            },
-            _ => println!("Se espera una opción válida")
+            }
+            _ => println!("Se espera una opción válida"),
         }
     }
 }
 
-fn create_text(text:&str) -> String{
+fn create_text(text: &str) -> String {
     loop {
         let mut input = String::new();
         println!("Ingresá el {}", text);
-        io::stdin().read_line(&mut input).expect("Error al ingresar");
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Error al ingresar");
         let input = input.trim().to_lowercase();
         if !input.is_empty() {
             return input;
@@ -67,7 +78,9 @@ fn create_number() -> u8 {
     loop {
         let mut input = String::new();
         println!("Ingresá la edad");
-        io::stdin().read_line(&mut input).expect("Error al ingresar");
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Error al ingresar");
         match input.trim().parse::<u8>() {
             Ok(number) => break number,
             Err(_) => {
@@ -81,10 +94,13 @@ fn create_number() -> u8 {
 fn create_user(users: &Vec<User>) -> User {
     let name = create_text("nombre");
     let lastname = create_text("apellido");
-    let age:u8 = create_number();
+    let age: u8 = create_number();
     let email = create_text("email");
 
-    let mut last_id:u8 = users.len().try_into().expect("El valor tiene más de 255 elementos");
+    let mut last_id: u8 = users
+        .len()
+        .try_into()
+        .expect("El valor tiene más de 255 elementos");
 
     last_id = last_id + 1;
 
@@ -94,7 +110,7 @@ fn create_user(users: &Vec<User>) -> User {
         lastname,
         age,
         email,
-        active: true
+        active: true,
     };
 }
 
@@ -103,16 +119,74 @@ fn list_users(users: &Vec<User>) {
         println!("No hay usuarios para listar");
     } else {
         for user in users {
+            if user.active {
+                println!("Id: {}", user.id);
+                println!("Nombre: {}", user.name);
+                println!("Apellido: {}", user.lastname);
+                println!("Edad: {}", user.age);
+                println!("Correo: {}", user.email);
+            }
+        }
+    }
+}
+
+fn list_users_active(users: &Vec<User>) {
+    if users.is_empty() {
+        println!("No hay usuarios activos para mostrar");
+    } else {
+        for user in users.iter().filter(|u| u.active) {
             println!("Id: {}", user.id);
             println!("Nombre: {}", user.name);
             println!("Apellido: {}", user.lastname);
             println!("Edad: {}", user.age);
             println!("Correo: {}", user.email);
-            if user.active {
-                println!("Estado: activo");
-            }else {
-                println!("Estado: inactivo");
+        }
+    }
+}
+
+fn list_users_inactive(users: &Vec<User>) {
+    for user in users.iter().filter(|u| !u.active) {
+        print!("Id: {}", user.id);
+        print!("Nombre: {}", user.name);
+        print!("Correo: {}", user.email);
+    }
+}
+
+fn find_user_by_id(id: u8, users: &Vec<User>) -> Option<&User> {
+    return users.iter().find(|user| user.id == id);
+}
+
+fn toggle_status(users: &mut Vec<User>) {
+    let mut input: String = String::new();
+    let mut menu_is_running: bool = true;
+
+    while menu_is_running {
+        println!("¿Que desea hacer?");
+        println!("1)- Activar usuario");
+        println!("2)- Inhabilitar usuario");
+        println!("0)- Volver al menú");
+
+        let option = loop {
+            input.clear();
+
+            io::stdin().read_line(&mut input).expect("Error al leer");
+            match input.trim().parse::<u8>() {
+                Ok(number) => break number,
+                Err(_) => {
+                    println!("Se espera un número");
+                    continue;
+                }
             }
+        };
+
+        match option {
+            1 => {
+                list_users_inactive(&users);
+            }
+            0 => {
+                menu_is_running = false;
+            }
+            _ => println!("Opción incorrecta"),
         }
     }
 }
